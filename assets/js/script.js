dcf985af0cdc898d204c5b2728b2d34ceaac9d6c
hje58raf <mjelavic@inet.hr>
2022-09-05 02:12:53 +0000

    Add java function for user and computer choise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,48 @@
-const choises = document.querySelectorAll(".choises");
 let yourScore = 0;
 let compScore = 0;
+const userWinResults = ['scissorspaper', 'paperrock', 'rocklizard', 'lizardspock', 'spockscissors',
+    'rockscissors', 'scissorslizard', 'lizardpaper', 'paperspock', 'spockrock'];
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const lizard = document.getElementById('lizard');
+const spock = document.getElementById('spock');
 
-choises.forEach((choise) => {
-    choise.addEventListener("click", function () {
-        const yourInput = this.textContent;
+rock.addEventListener('click', () => game('rock'));
+paper.addEventListener('click', () => game('paper'));
+scissors.addEventListener('click', () => game('scissors'));
+lizard.addEventListener('click', () => game('lizard'));
+spock.addEventListener('click', () => game('spock'));
 
-        const compChoises = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
-        const compInput = compOptions[Math.floor(Math.random() * 5)];
+function game(yourChoise) {
+    const compChoise = getcompInput();
+    console.log(yourChoise)
+    console.log(compChoise)
+    
+}
 
-        compareInputs(yourInput, compInput);
-        updateScore();
-        if (checkWinner()) {
-            yourScore = compScore = 0;
-            updateScore();
-        }
-    });
-});
+function getcompInput() {
+    const compChoise = ["rock", "paper", "scissors", "lizard", "spock"];
+    const randomNumber = [Math.floor(Math.random() * 5)] ;
+    return compChoise[randomNumber];
+    };
+
+
+
+function compareInputs(youInput, youInput) {
+    if (youInput === youInput) {
+        console.log("Tie")
+        return;
+    } else if (getUserWinsStatus(youInput + compInput)) {
+        yourScore++;
+        console.log("You win")
+    } else {
+        compScore++;
+        console.log('You lose')
+    }
+}
+
+function getUserWinsStatus(result) {
+    return userWinResults.some(winStr => winStr === result);
+}
+
