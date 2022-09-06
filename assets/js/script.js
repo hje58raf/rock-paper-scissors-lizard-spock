4643f4869b3eda8f19e9e23d79922845a7ea68b7
hje58raf <mjelavic@inet.hr>
2022-09-06 11:37:00 +0000

    Add game function to compare computer choice with user choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,43 @@
 let yourScore = 0;
 let compScore = 0;
-const userWinResults = ['scissorspaper', 'paperrock', 'rocklizard', 'lizardspock', 'spockscissors',
-    'rockscissors', 'scissorslizard', 'lizardpaper', 'paperspock', 'spockrock'];
+let gameRules = {
+    rock: {
+        rock: 'draw',
+        scissors: 'lose',
+        paper: 'win',
+        spock: 'win',
+        lizard: 'lose'
+    },
+    paper: {
+        paper: 'draw',
+        rock: 'lose',
+        scissors: 'win',
+        lizard: 'win',
+        spock: 'lose'
+    },
+    scissors: {
+        scissors: 'draw',
+        paper: 'lose',
+        rock: 'win',
+        lizard: 'lose',
+        spock: 'win'
+    },
+    lizard: {
+        scissors: 'win',
+        paper: 'lose',
+        rock: 'win',
+        lizard: 'draw',
+        spock: 'lose'
+    },
+    spock: {
+        scissors: 'lose',
+        paper: 'win',
+        rock: 'lose',
+        lizard: 'win',
+        spock: 'draw'
+    }
+}
+
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
@@ -14,35 +50,24 @@ scissors.addEventListener('click', () => game('scissors'));
 lizard.addEventListener('click', () => game('lizard'));
 spock.addEventListener('click', () => game('spock'));
 
+
 function game(yourChoise) {
-    const compChoise = getcompInput();
-    console.log(yourChoise)
-    console.log(compChoise)
-    
-}
+    const compInput = ["rock", "paper", "scissors", "lizard", "spock"];
+    const compChoise = compInput[[Math.floor(Math.random() * 5)]] ;
 
-function getcompInput() {
-    const compChoise = ["rock", "paper", "scissors", "lizard", "spock"];
-    const randomNumber = [Math.floor(Math.random() * 5)] ;
-    return compChoise[randomNumber];
-    };
-
-
-
-function compareInputs(youInput, youInput) {
-    if (youInput === youInput) {
-        console.log("Tie")
-        return;
-    } else if (getUserWinsStatus(youInput + compInput)) {
-        yourScore++;
-        console.log("You win")
-    } else {
-        compScore++;
-        console.log('You lose')
+    switch(gameRules[compChoise][yourChoise]) {
+        case 'win':
+            yourScore++;
+            console.log("You Win")
+            console.log(yourScore)
+            break;
+        case 'lose':
+            compScore++;
+            console.log("You lose")
+            console.log(compScore)
+            break;
+        case 'draw':
+            console.log("You draw")
+            break;   
     }
 }
-
-function getUserWinsStatus(result) {
-    return userWinResults.some(winStr => winStr === result);
-}
-
